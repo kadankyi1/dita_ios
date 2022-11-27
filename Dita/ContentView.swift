cf0893cdd58fd0d8a862232e77e0078afa875850
kadankyi1 <annodankyikwaku@gmail.com>
2022-11-27 02:39:17 -0800

    Initial Commit

---
+++ Dita/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dita
+//
+//  Created by Dankyi Anno Kwaku on 11/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dita/DitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DitaApp.swift
+//  Dita
+//
+//  Created by Dankyi Anno Kwaku on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct DitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
